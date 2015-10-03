61b76b07888f1187bbbd0fcfaf16c8aa4c1647b1
Derek Strickland <stri0039@algonquinlive.com>
2015-10-03 17:32:50 -0400

    added JS functionality

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,43 @@
 // JavaScript Document
+var longRect; 
+var shortRect;
+var boxCheck = false;
+
+window.onload = function()
+{
+longRect =  document.querySelector(".long-rect");
+shortRect =   document.querySelector(".short-rect");
+  console.log("Page is loaded.");
+
+addEvents();  
+}
+
+
+function addEvents(){
+   
+  
+        
+     longRect.addEventListener("mouseover", function( event ) {
+     shortRect.innerHTML = "over";
+  
+    if(boxCheck == false)
+      {
+        shortRect.addEventListener("click", function( event ) {
+        alert("Congratulation!");
+      });
+       
+     boxCheck = true;
+     }
+      });
+
+
+     
+    
+  
+   longRect.addEventListener("mouseout", function( event ) {
+     shortRect.innerHTML = "out";
+   });
+}
+
 
 
